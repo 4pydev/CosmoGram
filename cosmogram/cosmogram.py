377ba0c9cb52bb081b439ac6b48d1565ee9f366a
4pydev <4pydev@gmail.com>
2019-07-10 17:59:21 +0300

    Realized Hubble image downloading function

--- cosmogram/cosmogram.py
+++ cosmogram/cosmogram.py
@@ -2,17 +2,21 @@ import os
 import requests
 
 
-PICS_DIRECTORY = "./images"
+PICS_DIRECTORY = "../images"
 
 
-def create_images_dir():
+def check_images_dir():
     if not os.path.exists(PICS_DIRECTORY):
         os.makedirs(PICS_DIRECTORY)
 
 
-def load_image_from_url(filename, url):
-    create_images_dir()
-    response = requests.get(url)
+def get_img_file_extension(url):
+    return url.split(".")[-1]
+
+
+def download_img_from_url(filename, url):
+    check_images_dir()
+    response = requests.get(url, verify=False)
 
     with open(f"./{PICS_DIRECTORY}/{filename}", "wb") as img_file:
         img_file.write(response.content)
@@ -27,7 +31,19 @@ def fetch_spacex_last_launch():
     for index, img_link in enumerate(img_links):
         filename = f"spacex{index}.jpg"
 
-        load_image_from_url(filename, img_link)
+        download_img_from_url(filename, img_link)
+
+
+def fetch_hubble_img(img_id):
+    url = f"http://hubblesite.org/api/v3/image/{img_id}"
+    response = requests.get(url).json()
+
+    img_data = response["image_files"][-1]["file_url"]
+    img_url = f"http:{img_data}"
+
+    filename = f"hubble#{img_id}.{get_img_file_extension(img_url)}"
+
+    download_img_from_url(filename, img_url)
 
 
 def main():
